5f24e602e638751facd2d53d67fc7f4998bc08b9
KleberUchoa <150630675+KleberUchoa@users.noreply.github.com>
2025-07-30 22:30:25 +0000

    Implementa Inserção de navio na vertical

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-int tabuleiro[10][10] = { 
+#define TAM 10
+
+int tabuleiro[TAM][TAM] = { 
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -17,12 +19,13 @@ int tabuleiro[10][10] = {
 
 int navio1[3] = {3, 3, 3};
 int navio2 [4] = {3, 3, 3, 3};
+int navio3[5] = {3, 3, 3, 3, 3};
 
 int espacoVazios()
 {   int contador = 0;
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < TAM; i++)
     {
-        for(int j = 0; j < 10; j++)
+        for(int j = 0; j < TAM; j++)
         {
             if(tabuleiro[i][j] == 0)
                 contador++;
@@ -34,10 +37,10 @@ int espacoVazios()
 
 void ImprimeTabuleiro()
 {
-    for(int i = 0; i < 10; i++)
+    for(int i = 0; i < TAM; i++)
     {
         printf("%d\t", i + 1);
-        for(int j = 0; j < 10; j++)
+        for(int j = 0; j < TAM; j++)
         {
             printf("%d ", tabuleiro[j][i]);
         }
@@ -45,20 +48,20 @@ void ImprimeTabuleiro()
     }
     printf("-\t");
     char letra = 'A';
-    for (size_t i = 0; i < 10; i++)
+    for (size_t i = 0; i < TAM; i++)
     {
         printf("%c ", letra);
         letra++;
     }
     
 }
-int ChecaEspacoNaHorizontal(int posX, int posY, int tamanho)
+int ChecaEspacoNaHorizontal(int posx, int posy, int tamanho)
 {
-    if(posX + tamanho < 10)
+    if(posx + tamanho < TAM)
     {
         for(int i = 0; i < tamanho; i++)
         {
-            if(tabuleiro[posX + i][posY] == 3)
+            if(tabuleiro[posx + i][posy] == 3)
             {
                 return 0;
             }
@@ -71,33 +74,37 @@ int ChecaEspacoNaHorizontal(int posX, int posY, int tamanho)
 void PoeNavioNaHorizontal(int *navio, int tamanho)
 {
     int tentativas = 10;
-    
-    int posX = rand() % 10;
-    int posY = rand() % 10;
-    
-    //imprime na horizontal
-    if(ChecaEspacoNaHorizontal(posX, posY, tamanho))
+    while (tentativas -- > 0)
     {
-        for(int i = 0; i < tamanho; i++)
+        int posx = rand() % TAM;
+        int posy = rand() % TAM;
+    
+        //imprime na horizontal
+        if(ChecaEspacoNaHorizontal(posx, posy, tamanho))
         {
-            tabuleiro[posX + i] [posY] = navio[i]; // 3 representa o navio, 0 a água
-        }
-    }else {
-        if(tamanho < espacoVazios() && tentativas > 0)
+            for(int i = 0; i < tamanho; i++)
+            {
+                tabuleiro[posx + i] [posy] = navio[i]; // 3 representa o navio, 0 a água
+            }
+            return;
+        }else 
         {
-            tentativas--;
-            PoeNavioNaHorizontal(navio, tamanho);
-        }
+            if(tentativas < 0)
+                printf("Não foi possível inserio o návio");
         
-     }
+        }
+    }
+    
+    
+    
 }
-int ChecaEspacoNaVertical(int posX, int posY, int tamanho)
+int ChecaEspacoNaVertical(int posx, int posy, int tamanho)
 {
-    if(posY + tamanho < 10)
+    if(posy + tamanho < TAM)
     {
         for(int i = 0; i < tamanho; i++)
         {
-             if(tabuleiro[posX][posY + i] == 3)
+             if(tabuleiro[posx][posy + i] == 3)
             {
                 return 0;
             }
@@ -110,32 +117,79 @@ int ChecaEspacoNaVertical(int posX, int posY, int tamanho)
 void PoeNavioNaVertical(int *navio, int tamanho)
 {
     int tentativas = 10;
-    
-    int posX = rand() % 10;
-    int posY = rand() % 10;
-    
-    //imprime na vertical
-    if(ChecaEspacoNaVertical(posX, posY, tamanho)) //checa se há espaço para o navio a partir da posição gerada aleatoriamente
+    while (tentativas -- > 0)
     {
+        int posx = rand() % TAM;
+        int posy = rand() % TAM;
+    
+        //imprime na vertical
+        if(ChecaEspacoNaVertical(posx, posy, tamanho)) //checa se há espaço para o navio a partir da posição gerada aleatoriamente
+        {
+            for(int i = 0; i < tamanho; i++)
+            {
+                //Imprime no tabuleiro, o navio na posição vertical
+                tabuleiro[posx] [posy + i] = navio[i]; // 3 representa o navio, 0 a água
+            }
+            return; //após imprimir encerra o laço
+        }
+        else 
+        {
+            if(tentativas == 0) 
+                printf("Não foi possível inserir o návio");
+        }   
+    }
+    
+    
+}
+int ChecaEspacoNaDiagonal(int posx, int posy, int tamanho, int dx, int dy)//dx e dy admite 1 ou -1
+{
+    int nx = posx;
+    int ny = posy;
+       
         for(int i = 0; i < tamanho; i++)
         {
-            //Imprime no tabuleiro, o navio na posição vertical
-            tabuleiro[posX] [posY + i] = navio[i]; // 3 representa o navio, 0 a água
+            if(nx < 0 || nx >= TAM  || ny < 0 || ny >= TAM) return 0;
+            if(tabuleiro[nx][ny] == 3) return 0;
+            nx += dx;
+            ny += dy;
         }
-    }else {
-        if(tamanho < espacoVazios() && tentativas > 0) //os espaços vazios precisam ser maiores que o navio
+    return 1;
+}
+//direção dx e dy suportam apenas um 1 ou -1
+void PoeNavioNaDiagonal(int *navio, int tamanho, int dx, int dy) 
+{
+    int tentativas = 10;
+    while (tentativas-- > 0)
+    {
+        int posx = rand() % TAM;
+        int posy = rand() % TAM;
+        if(ChecaEspacoNaDiagonal(posx, posy, tamanho, dx, dy))
         {
-            tentativas--; //dez tentativas no máximo, evita erro de loop infinito
-            PoeNavioNaVertical(navio, tamanho); //tenta inserir novamente o navio se falhar na primeira tentativa
+            for(int i = 0; i < tamanho; i++)
+            {
+                tabuleiro[posx][posy] = 3;
+                posx += dx;
+                posy += dy;
+            }
+            return;
+        } else{
+            if(tentativas == 0){
+                printf("Não foi possível inserir o navio!\n");
+            }
         }
-        
-     }
+    }
+    
+    
+    
+
+    
 }
 
 int main() {
     srand(time(0)); //Inicia a geração de numeros aleatórios a partir do horário de execução
     PoeNavioNaHorizontal(navio1, 3);
     PoeNavioNaVertical (navio2, 4);
+    PoeNavioNaDiagonal(navio3, 5, 1, -1);
     ImprimeTabuleiro();
     return 0;
 }
